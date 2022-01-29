7e39b2ae4152f844b0948faa832d424a8bed943c
engrab <engrbilawalhussain@gmail.com>
2022-01-29 18:04:37 +0500

    adding checkout for multiple devices

---
+++ app/src/main/java/com/example/recetteapp/DrinkFragment.java
@@ -0,0 +1,237 @@
+package com.example.recetteapp;
+
+import static com.example.recetteapp.InternetConnection.checkConnection;
+
+import android.app.ProgressDialog;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DrinkFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DrinkFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private TextView noInternet;
+    private RecyclerView rv;
+    private ProductArrayAdapter adapter;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DrinkFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DrinkFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DrinkFragment newInstance(String param1, String param2) {
+        DrinkFragment fragment = new DrinkFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflate = inflater.inflate(R.layout.fragment_drink, container, false);
+        rv = inflate.findViewById(R.id.rv);
+        noInternet = inflate.findViewById(R.id.tvNoInternet);
+        return inflate;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
+        if (checkConnection(requireActivity())){
+            noInternet.setVisibility(View.GONE);
+            new GetProducts().execute();
+            adapter = new ProductArrayAdapter(requireContext(), Utils.productList);
+
+
+            int orientation = this.getResources().getConfiguration().orientation;
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                rv.setLayoutManager(new GridLayoutManager(requireContext(), 1));
+                rv.setAdapter(adapter);
+
+            } else {
+                rv.setLayoutManager(new GridLayoutManager(requireContext(), 2));
+                rv.setAdapter(adapter);
+
+            }
+        }else {
+            noInternet.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    class GetProducts extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog dialog;
+        int jIndex;
+        int x;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            /**
+             * Progress Dialog for User Interaction
+             */
+            Utils.productList.clear();
+            x = Utils.productList.size();
+
+            if (x == 0)
+                jIndex = 0;
+            else
+                jIndex = x;
+
+            dialog = new ProgressDialog(requireContext());
+            dialog.setTitle("Hey Wait Please..." );
+            dialog.setMessage("I am getting your Data");
+            dialog.setCancelable(false);
+            dialog.show();
+        }
+
+        @Nullable
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            /**
+             * Getting JSON Object from Web Using okHttp
+             */
+            JSONObject jsonObject = JSONparser.getProductFromWeb();
+
+            try {
+                /**
+                 * Check Whether Its NULL???
+                 */
+                if (jsonObject != null) {
+                    /**
+                     * Check Length...
+                     */
+                    if (jsonObject.length() > 0) {
+                        /**
+                         * Getting Array named "contacts" From MAIN Json Object
+                         */
+                        JSONArray array = jsonObject.getJSONArray(Keys.SHEET_PRODUCTS);
+
+                        /**
+                         * Check Length of Array...
+                         */
+
+
+                        int lenArray = array.length();
+                        if (lenArray > 0) {
+
+                            for (; jIndex < lenArray; jIndex++) {
+
+                                /**
+                                 * Creating Every time New Object
+                                 * and
+                                 * Adding into List
+                                 */
+                                ProductModel model = new ProductModel();
+
+                                /**
+                                 * Getting Inner Object from contacts array...
+                                 * and
+                                 * From that We will get Name of that Contact
+                                 *
+                                 */
+                                JSONObject innerObject = array.getJSONObject(jIndex);
+//                                String name = innerObject.getString(Keys.KEY_NAME);
+//                                String country = innerObject.getString(Keys.KEY_COUNTRY);
+
+                                model.setId(innerObject.getString(Keys.KEY_PRODUCTS_ID));
+                                model.setName(innerObject.getString(Keys.KEY_PRODUCTS_NAME));
+                                model.setImages(innerObject.getString(Keys.KEY_PRODUCTS_IMAGES));
+                                model.setPrice(innerObject.getString(Keys.KEY_PRODUCTS_PRICE));
+                                model.setQuantity(innerObject.getString(Keys.KEY_PRODUCTS_QUANTITY));
+
+
+                                /**
+                                 * Getting Object from Object "phone"
+                                 */
+                                //JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
+                                //String phone = phoneObject.getString(Keys.KEY_MOBILE);
+
+//                                model.setName(name);
+//                                model.setCountry(country);
+
+                                /**
+                                 * Adding name and phone concatenation in List...
+                                 */
+                                Utils.productList.add(model);
+                            }
+                        }
+                    }
+                } else {
+
+                }
+            } catch (JSONException je) {
+                Log.i(JSONparser.TAG, "" + je.getLocalizedMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            dialog.dismiss();
+            /**
+             * Checking if List size if more than zero then
+             * Update ListView
+             */
+            if (Utils.productList.size() > 0) {
+                adapter.notifyDataSetChanged();
+
+
+
+            } else {
+                Toast.makeText(requireContext(), "No Data Found", Toast.LENGTH_SHORT).show();            }
+        }
+    }
+}
--- app/src/main/java/com/example/recetteapp/MainActivity.java
+++ app/src/main/java/com/example/recetteapp/MainActivity.java
@@ -1,43 +1,26 @@
 package com.example.recetteapp;
 
 
-import static com.example.recetteapp.InternetConnection.checkConnection;
-
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.ViewTarget;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView rv;
-    private ProductArrayAdapter adapter;
+
     private static final String TAG = MainActivity.class.getName();
-    private TextView noInternet;
+
+    private String[] titles = new String[]{"Food", "Drink"};
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
 
 
     @Override
@@ -45,163 +28,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rv = findViewById(R.id.rv);
-        noInternet = findViewById(R.id.tvNoInternet);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tab_layout);
 
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-
-        if (checkConnection(this)){
-            noInternet.setVisibility(View.GONE);
-            new GetProducts().execute();
-            adapter = new ProductArrayAdapter(this, Utils.productList);
-
-
-            int orientation = this.getResources().getConfiguration().orientation;
-            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-                rv.setLayoutManager(new GridLayoutManager(this, 1));
-                rv.setAdapter(adapter);
-
-            } else {
-                rv.setLayoutManager(new GridLayoutManager(this, 2));
-                rv.setAdapter(adapter);
-
-            }
-        }else {
-            noInternet.setVisibility(View.VISIBLE);
-        }
+        init();
 
     }
 
-    class GetProducts extends AsyncTask<Void, Void, Void> {
+    private void init() {
+        // removing toolbar elevation
+        getSupportActionBar().setElevation(0);
 
-        ProgressDialog dialog;
-        int jIndex;
-        int x;
+        viewPager.setAdapter(new ViewPagerFragmentAdapter(this));
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            /**
-             * Progress Dialog for User Interaction
-             */
-            Utils.productList.clear();
-            x = Utils.productList.size();
+        // attaching tab mediator
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText(titles[position])).attach();
+    }
 
-            if (x == 0)
-                jIndex = 0;
-            else
-                jIndex = x;
+    private class ViewPagerFragmentAdapter extends FragmentStateAdapter {
 
-            dialog = new ProgressDialog(MainActivity.this);
-            dialog.setTitle("Hey Wait Please..." );
-            dialog.setMessage("I am getting your Data");
-            dialog.setCancelable(false);
-            dialog.show();
+        public ViewPagerFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
         }
 
-        @Nullable
+        @NonNull
         @Override
-        protected Void doInBackground(Void... params) {
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new FoodFragment();
+                case 1:
+                    return new DrinkFragment();
 
-            /**
-             * Getting JSON Object from Web Using okHttp
-             */
-            JSONObject jsonObject = JSONparser.getProductFromWeb();
-
-            try {
-                /**
-                 * Check Whether Its NULL???
-                 */
-                if (jsonObject != null) {
-                    /**
-                     * Check Length...
-                     */
-                    if (jsonObject.length() > 0) {
-                        /**
-                         * Getting Array named "contacts" From MAIN Json Object
-                         */
-                        JSONArray array = jsonObject.getJSONArray(Keys.SHEET_PRODUCTS);
-
-                        /**
-                         * Check Length of Array...
-                         */
-
-
-                        int lenArray = array.length();
-                        if (lenArray > 0) {
-
-                            for (; jIndex < lenArray; jIndex++) {
-
-                                /**
-                                 * Creating Every time New Object
-                                 * and
-                                 * Adding into List
-                                 */
-                                ProductModel model = new ProductModel();
-
-                                /**
-                                 * Getting Inner Object from contacts array...
-                                 * and
-                                 * From that We will get Name of that Contact
-                                 *
-                                 */
-                                JSONObject innerObject = array.getJSONObject(jIndex);
-//                                String name = innerObject.getString(Keys.KEY_NAME);
-//                                String country = innerObject.getString(Keys.KEY_COUNTRY);
-
-                                model.setId(innerObject.getString(Keys.KEY_PRODUCTS_ID));
-                                model.setName(innerObject.getString(Keys.KEY_PRODUCTS_NAME));
-                                model.setImages(innerObject.getString(Keys.KEY_PRODUCTS_IMAGES));
-                                model.setPrice(innerObject.getString(Keys.KEY_PRODUCTS_PRICE));
-                                model.setQuantity(innerObject.getString(Keys.KEY_PRODUCTS_QUANTITY));
-
-
-                                /**
-                                 * Getting Object from Object "phone"
-                                 */
-                                //JSONObject phoneObject = innerObject.getJSONObject(Keys.KEY_PHONE);
-                                //String phone = phoneObject.getString(Keys.KEY_MOBILE);
-
-//                                model.setName(name);
-//                                model.setCountry(country);
-
-                                /**
-                                 * Adding name and phone concatenation in List...
-                                 */
-                                Utils.productList.add(model);
-                            }
-                        }
-                    }
-                } else {
-
-                }
-            } catch (JSONException je) {
-                Log.i(JSONparser.TAG, "" + je.getLocalizedMessage());
             }
-            return null;
+            return new FoodFragment();
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            dialog.dismiss();
-            /**
-             * Checking if List size if more than zero then
-             * Update ListView
-             */
-            if (Utils.productList.size() > 0) {
-                adapter.notifyDataSetChanged();
-
-
-
-            } else {
-                Toast.makeText(MainActivity.this, "No Data Found", Toast.LENGTH_SHORT).show();            }
+        public int getItemCount() {
+            return titles.length;
         }
     }
 
